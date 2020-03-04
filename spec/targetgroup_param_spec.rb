c53ec8476a31cd42bc48935a06bee8425ba1d842
Aaron Walker <aaronpwalker@gmail.com>
2020-03-04 18:33:59 +0100

    adds spec for test passing target group as a template param

---
+++ spec/targetgroup_param_spec.rb
@@ -0,0 +1,42 @@
+require 'yaml'
+
+describe 'compiled component' do
+  
+  context 'cftest' do
+    it 'compiles test' do
+      expect(system("cfhighlander cftest #{@validate} --tests tests/targetgroup_param.test.yaml")).to be_truthy
+    end      
+  end
+
+  let(:template) { YAML.load_file("#{File.dirname(__FILE__)}/../out/tests/targetgroup_param/fargate-v2.compiled.yaml") }
+
+  context 'check template parameters' do
+    
+    let(:parameters) { template["Parameters"] }
+
+    it 'has load balancer params' do
+      expect(parameters).to include({
+        "LoadBalancer" => {"Default"=>"", "NoEcho"=>false, "Type"=>"String"},
+        "DnsDomain" => {"Default"=>"", "NoEcho"=>false, "Type"=>"String"},
+        "Listener" => {"Default"=>"", "NoEcho"=>false, "Type"=>"String"},
+        "TargetGroup" => {"Default"=>"", "NoEcho"=>false, "Type"=>"String"}
+      })
+    end
+
+    context 'Resource TargetRule' do
+
+      let(:loadbalancer) { template["Resources"]["Service"]["Properties"]["LoadBalancers"] }
+  
+      it 'has property Properties' do
+        expect(loadbalancer).to eq([{
+          "ContainerName"=>"nginx", 
+          "ContainerPort"=>80, 
+          "TargetGroupArn"=>{"Ref"=>"TargetGroup"}
+        }])
+      end
+  
+    end
+
+  end
+
+end
