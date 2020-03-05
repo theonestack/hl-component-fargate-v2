809426914d1a5e600c186fc12ecd4f0df8de155c
Aaron Walker <aaronpwalker@gmail.com>
2020-03-05 16:30:00 +0100

    need to add suffix to sub component name

--- fargate-v2.cfhighlander.rb
+++ fargate-v2.cfhighlander.rb
@@ -26,6 +26,6 @@ CfhighlanderTemplate do
   end
 
   #Pass the all the config from the parent component to the inlined component
-  Component template: 'ecs-task@secrets', name: "#{component_name.gsub('-','').gsub('_','')}", render: Inline, config: @config
+  Component template: 'ecs-task@secrets', name: "#{component_name.gsub('-','').gsub('_','')}Task", render: Inline, config: @config
 
 end
