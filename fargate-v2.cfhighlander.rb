086da001b34f2cb3b535db364d0ef1e0eef3bfb7
Tarun Menon <tarunmenon95@gmail.com>
2024-07-22 10:59:57 +1000

    fix path

--- fargate-v2.cfhighlander.rb
+++ fargate-v2.cfhighlander.rb
@@ -44,7 +44,7 @@ CfhighlanderTemplate do
   end
 
   #Pass the all the config from the parent component to the inlined component
-  Component template: 'git:https://github.com/theonestack/hl-component-ecs-task#feature/ebs_support.snapshot', name: "#{component_name.gsub('-','').gsub('_','')}Task", render: Inline, config: @config do
+  Component template: 'git:https://github.com/theonestack/hl-component-ecs-task#feature/ebs-support.snapshot', name: "#{component_name.gsub('-','').gsub('_','')}Task", render: Inline, config: @config do
     parameter name: 'DnsDomain', value: Ref('DnsDomain')
   end
 
