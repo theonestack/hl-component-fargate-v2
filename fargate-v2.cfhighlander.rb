9b7497baa15f3d703e05a9d8c202d7505bd63e40
Tarun Menon <tarunmenon95@gmail.com>
2024-08-27 11:54:21 +1000

    manually wire tenant to ecs-task

--- fargate-v2.cfhighlander.rb
+++ fargate-v2.cfhighlander.rb
@@ -46,6 +46,7 @@ CfhighlanderTemplate do
   #Pass the all the config from the parent component to the inlined component
   Component template: 'ecs-task@0.5.10', name: "#{component_name.gsub('-','').gsub('_','')}Task", render: Inline, config: @config do
     parameter name: 'DnsDomain', value: Ref('DnsDomain')
+    parameter name: 'Tenant', value: Ref('Tenant')
   end
 
   unless service_namespace.nil?
