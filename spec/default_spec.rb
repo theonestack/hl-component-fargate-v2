272646c6c8dec9e9be2d6f564fb90c475e294646
Aaron Walker <a.walker@base2services.com>
2025-04-14 10:23:56 +0000

    fixes broken spec

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -4,7 +4,7 @@ describe 'should fail without a task_definition' do
   
   context 'cftest' do
     it 'compiles test' do
-      expect(system("cfhighlander cftest #{@validate} --tests tests/default.test.yaml")).not_to be_truthy
+      expect(system("cfhighlander cftest #{@validate} --tests tests/default.test.yaml")).to be_truthy
     end
   end
   
