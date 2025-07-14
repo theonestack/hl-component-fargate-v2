c7f4356cb950b95a9afb6b122718f816a7c4b702
petlitskiy <81190543+petlitskiy@users.noreply.github.com>
2025-07-14 14:54:41 +0300

    Use versionConsistency

--- fargate-v2.cfhighlander.rb
+++ fargate-v2.cfhighlander.rb
@@ -45,7 +45,7 @@ CfhighlanderTemplate do
   end
 
   #Pass the all the config from the parent component to the inlined component
-  Component template: 'ecs-task@0.5.12', name: "#{component_name.gsub('-','').gsub('_','')}Task", render: Inline, config: @config do
+  Component template: 'ecs-task@0.5.13', name: "#{component_name.gsub('-','').gsub('_','')}Task", render: Inline, config: @config do
     parameter name: 'DnsDomain', value: Ref('DnsDomain')
     
     additional_parameters.each do |parameter_name|
